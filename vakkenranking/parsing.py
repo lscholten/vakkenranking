7eabe99d18f1bd55b9f098f5839dcda88528da3b
Luuk Scholten <info@luukscholten.com>
2016-05-15 09:43:46 +0200

    Add recursive opening of xls evaluations
    Now the course evaluations don't have to be converted
    to CSV with a command. Makes the whole process a lot
    easier.

--- vakkenranking/parsing.py
+++ vakkenranking/parsing.py
@@ -2,11 +2,12 @@ import pandas as pd
 import numpy as np
 import re
 import os
+import sys
 
 
 class Parser(object):
 
-    _filename_rule = ".*_(?:NWI-)?([^\\_^-]+)_(.+)_([0-9]+)_(.+)\\.csv"
+    _filename_rule = ".*_(?:NWI-)?([^\\_^-]+)_(.+)_([0-9]+)_(.+)\\.xls"
 
     _teachergrade_keys = [
         "I would rate the performance of the lecturer\\(s\\)/teacher\\(s\\) as \\[(.+)\\]",
@@ -36,14 +37,14 @@ class Parser(object):
         df_keys = list(dataframe.keys())
         keys = [x for x in self._coursegrade_keys if x in df_keys]
 
-        grades = [[x  for x in dataframe[k] if not np.isnan(x)] for k in keys]
+        grades = [[x for x in dataframe[k] if not np.isnan(x)] for k in keys]
         return grades
 
     def _parse_teachergrades(self, dataframe):
         df_keys = list(dataframe.keys())
         keys = [x for x in df_keys for y in self._teachergrade_keys if re.match(y, x)]
 
-        grades = [[x  for x in dataframe[k] if not np.isnan(x)] for k in keys]
+        grades = [[x for x in dataframe[k] if not np.isnan(x)] for k in keys]
         return grades
 
     def parse(self, path):
@@ -51,15 +52,21 @@ class Parser(object):
             print("Error: folder could not be read")
             exit(1)
 
+        # Temporarily catch stdout to suppress xlrd warnings
+        sysout = sys.stdout
+        sys.stdout = open(os.devnull, 'w')
+
         courses = {}
 
-        for filename in os.listdir(path):
-            course = self._parse_filename(filename)
-            df = pd.read_csv(os.path.join(path, filename), encoding='ISO-8859-1')
-            course['course_grades']  = self._parse_coursegrade(df)
-            course['teacher_grades'] = self._parse_teachergrades(df)
+        for root, subdir, files in os.walk(path):
+            xls = [x for x in files if x.endswith('xls')]
+            for filename in xls:
+                course = self._parse_filename(filename)
+                df = pd.read_excel(os.path.join(root, filename), encoding='ISO-8859-1')
+                course['course_grades'] = self._parse_coursegrade(df)
+                course['teacher_grades'] = self._parse_teachergrades(df)
 
-            courses[course['code']] = course
+                courses[course['code']] = course
 
-
+        sys.stdout = sysout
         return courses
