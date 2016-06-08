d24a33941227c4be366e4c47f7fed5d8217db1c3
Luuk Scholten <info@luukscholten.com>
2016-06-08 12:42:00 +0200

    Add csv rendering

--- vakkenranking/rendering.py
+++ vakkenranking/rendering.py
@@ -1,11 +1,44 @@
 from datetime import date
 from jinja2 import Environment, PackageLoader
+import pandas as pd
+import csv
 
 
 class CSVRenderer(object):
 
     def render(self, courses):
-        return "Not yet implemented!"
+        courselist = [{**course, **{'code': code}} for code, course in courses.items()]
+        sortedcourses = sorted(
+            courselist,
+            key=lambda x: (x['stats']['course']['mean'], x['stats']['course']['n']),
+            reverse=True
+        )
+
+        f = lambda x: "{0:.2f}".format(x)
+        fn = lambda x: int(x)
+        df = pd.DataFrame({
+            'code': [str(c['code']) for c in sortedcourses],
+            'mean': [f(c['stats']['course']['mean']) for c in sortedcourses],
+            'std': [f(c['stats']['course']['std']) for c in sortedcourses],
+            'n': [fn(c['stats']['course']['n']) for c in sortedcourses],
+            'teachers_mean': [f(c['stats']['teachers']['mean']) for c in sortedcourses],
+            'teachers_std': [f(c['stats']['teachers']['std']) for c in sortedcourses],
+            'teachers_n': [fn(c['stats']['teachers']['n']) for c in sortedcourses],
+            'old_mean': [f(c['old_stats']['course']['mean']) if 'old_stats' in c else '' for c in sortedcourses],
+            'old_std': [f(c['old_stats']['course']['std']) if 'old_stats' in c else '' for c in sortedcourses],
+            'old_n': [fn(c['old_stats']['course']['n']) if 'old_stats' in c else '' for c in sortedcourses],
+        })
+
+        csv_str = df.to_csv(None,
+            columns=['code', 'mean', 'std', 'n',
+                     'teachers_mean', 'teachers_std', 'teachers_n',
+                     'old_mean', 'old_std', 'old_n'],
+            index=False,
+            header=False,
+            quoting=csv.QUOTE_ALL
+        )
+
+        return csv_str
 
 
 class HtmlRenderer(object):
